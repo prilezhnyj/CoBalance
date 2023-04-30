081187580ecac9523e981043e155dbddbd4416fa
Maksim Botalov <dev.botalov@gmail.com>
2023-04-30 22:57:36 +0500

    Initial Commit

---
+++ CoBalance/CoBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoBalanceApp.swift
+//  CoBalance
+//
+//  Created by Максим Боталов on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
